42c579d6ee6249d5d19fd42c88891b2a2a2d732b
Hoang Minh Huy <72985424+hmhuy0@users.noreply.github.com>
2025-05-29 20:46:14 +0800

    update

--- configs/kitchen-m.py
+++ configs/kitchen-m.py
@@ -4,13 +4,13 @@ import ml_collections
 def get_config():
     config = ml_collections.ConfigDict()
 
-    config.weight_decay = 0.1
+    config.weight_decay = 0.0
     config.actor_Q_scale = 1.5
-    config.v_beta = 30.0
-    config.r_scale = 5.0
+    config.v_beta = 20.0
+    config.r_scale = 2.0
     config.r_alpha = 0.3
-    config.tau = 0.03
-    config.state_norm = False
+    config.tau = 0.005
+    config.state_norm = True
  
     
     return config
